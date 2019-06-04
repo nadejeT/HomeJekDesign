2d2c5612c073f8bf1a5cbfe0e936067f79488173
Nadeje <tabibnadeje@gmail.com>
2019-06-04 04:44:03 +0100

    2nd commit

--- app/src/main/java/com/homejek/ui/MainActivity.java
+++ app/src/main/java/com/homejek/ui/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -17,14 +18,20 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_accueil:
+                    getSupportFragmentManager().beginTransaction().addToBackStack("fragment").replace(R.id.container, new fragments.Home()).commit();
+                   item.setChecked(true);
+                   Log.d("Erreur","Loading Home");
+                    Log.i("Fragment","Profile");
+                   // return true;
+                case R.id.navigation_achat:
+                    //mTextMessage.setText(R.string.title_achat);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_about:
+                   // mTextMessage.setText(R.string.title_about);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_profile:
+                    getSupportFragmentManager().beginTransaction().addToBackStack("fragment").replace(R.id.container, new fragments.Profile()).commit();
                     return true;
             }
             return false;
@@ -39,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (savedInstanceState == null) {
+            navigation.setSelectedItemId(R.id.navigation_accueil); // change to whichever id should be default
+            navigation.getMenu().findItem(R.id.navigation_accueil).setChecked(true);
+        }
     }
 
 }
--- app/src/main/java/fragments/Profile.java
+++ app/src/main/java/fragments/Profile.java
@@ -7,103 +7,49 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+import Adapter.ProfileAdapter;
+import Models.ProfileModel;
 import com.homejek.ui.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Profile.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Profile#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Profile extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ProfileAdapter profileAdapter;
+    private RecyclerView recyclerview;
+    private ArrayList<ProfileModel> profileModelArrayList;
 
-    private OnFragmentInteractionListener mListener;
-
-    public Profile() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Profile.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Profile newInstance(String param1, String param2) {
-        Profile fragment = new Profile();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    Integer inbox[]={R.drawable.ic_heart,R.drawable.ic_heart,R.drawable.ic_heart,R.drawable.ic_star,R.drawable.ic_heart,R.drawable.ic_heart};
+    Integer arrow[]={R.drawable.ic_chevron_right_black_24dp,R.drawable.ic_chevron_right_black_24dp,R.drawable.ic_chevron_right_black_24dp,R.drawable.ic_chevron_right_black_24dp,R.drawable.ic_chevron_right_black_24dp,R.drawable.ic_chevron_right_black_24dp};
+    String txttrades[]={"Mon Mot de passe","Mon adresse mail","Mon numéro de telephone","Ma date de naissance","Mon Profil","Deconnexion"};
+    String txthistory[]={"Modifier mon mot de passe","Modifier mon adresse mail","Modifier mon numéro de telephone","Modifier ma date de naissance","Modifier mon profil complet","Aurevoir HomeJek!"};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        recyclerview = v.findViewById(R.id.recycler1);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(v.getContext());
+        recyclerview.setLayoutManager(layoutManager);
+        recyclerview.setItemAnimator(new DefaultItemAnimator());
+
+        profileModelArrayList = new ArrayList<>();
+
+        for (int i = 0; i < inbox.length; i++) {
+            ProfileModel view = new ProfileModel(inbox[i],arrow[i],txttrades[i],txthistory[i]);
+            profileModelArrayList.add(view);
         }
+        profileAdapter = new ProfileAdapter(v.getContext(),profileModelArrayList);
+        recyclerview.setAdapter(profileAdapter);
+
+        return v;
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
