af802713884f89f59fa87563512bd04ae17808c8
Nadeje <tabibnadeje@gmail.com>
2019-06-06 23:30:25 +0100

    Profil et Home

---
+++ app/src/main/java/Adapter/ProfileAdapter.java
@@ -0,0 +1,63 @@
+package Adapter;
+import android.content.Context;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.homejek.ui.R;
+
+import java.util.ArrayList;
+
+import Models.ProfileModel;
+
+public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHolder> {
+    Context context;
+    private ArrayList<ProfileModel> profileModelArrayList;
+
+    public ProfileAdapter(Context context, ArrayList<ProfileModel> profileModelArrayList) {
+        this.context = context;
+        this.profileModelArrayList = profileModelArrayList;
+    }
+
+
+    @NonNull
+    @Override
+    public ProfileAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_profile,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProfileAdapter.ViewHolder holder, int position) {
+
+        holder.inbox.setImageResource(profileModelArrayList.get(position).getInbox());
+        holder.arrow.setImageResource(profileModelArrayList.get(position).getArrow());
+        holder.txttrades.setText(profileModelArrayList.get(position).getTxttrades());
+        holder.txthistory.setText(profileModelArrayList.get(position).getTxthistory());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return profileModelArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView inbox,arrow;
+        TextView txttrades,txthistory;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            inbox=itemView.findViewById(R.id.inbox);
+            arrow=itemView.findViewById(R.id.arrow);
+            txttrades=itemView.findViewById(R.id.txttrades);
+            txthistory=itemView.findViewById(R.id.txthistory);
+        }
+    }
+}
---
+++ app/src/main/java/Models/HomeListView.java
@@ -0,0 +1,39 @@
+package Models;
+
+public class HomeListView {
+
+    private int image;
+    private  String title;
+    private String description;
+
+
+    public HomeListView(int image, String title, String description) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
